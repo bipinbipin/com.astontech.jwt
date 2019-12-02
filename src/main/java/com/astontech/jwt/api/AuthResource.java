efe9061306f12b4791c876f5684ab8b3be8e6662
bipin <bipinbipin@gmail.com>
2019-12-02 12:48:26 -0600

    /authenticate endpoint setup test with postman

---
+++ src/main/java/com/astontech/jwt/api/AuthResource.java
@@ -0,0 +1,50 @@
+package com.astontech.jwt.api;
+
+import com.astontech.jwt.models.AuthenticationRequest;
+import com.astontech.jwt.models.AuthenticationResponse;
+import com.astontech.jwt.services.InMemUserDetailsService;
+import com.astontech.jwt.util.JwtUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthResource {
+
+    private AuthenticationManager authenticationManager;
+    private JwtUtil jwtUtil;
+    private InMemUserDetailsService inMemUserDetailsService;
+
+    public AuthResource(AuthenticationManager authenticationManager, JwtUtil jwtUtil, InMemUserDetailsService inMemUserDetailsService) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+        this.inMemUserDetailsService = inMemUserDetailsService;
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        }
+        catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+
+
+        final UserDetails userDetails = inMemUserDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+
+}
